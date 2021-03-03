a1bbc5e002c9cc58a740ecefb184f55910cfb80b
Matthew Bradshaw <mbradsh3@binghamton.edu>
2021-03-03 09:44:56 -0500

    tutorial on animation from lecture

--- Drawing/Drawing/DrawingSpace.h
+++ Drawing/Drawing/DrawingSpace.h
@@ -10,6 +10,7 @@
 NS_ASSUME_NONNULL_BEGIN
 
 @interface DrawingSpace : UIView
+@property (nonatomic) float touch_x, touch_y;
 
 @end
 
--- Drawing/Drawing/DrawingSpace.m
+++ Drawing/Drawing/DrawingSpace.m
@@ -8,13 +8,31 @@
 #import "DrawingSpace.h"
 
 @implementation DrawingSpace
+@synthesize touch_x, touch_y;       // allows set or get the touches
 
-/*
 // Only override drawRect: if you perform custom drawing.
 // An empty implementation adversely affects performance during animation.
 - (void)drawRect:(CGRect)rect {
     // Drawing code
+    NSLog(@"Redrew");
+    CGRect bounds = [self bounds];
+    CGContextRef context = UIGraphicsGetCurrentContext();
+    
+    [[UIColor blueColor] setFill];
+    CGContextFillRect(context, bounds);
+    
+    [[UIColor whiteColor] setFill];
+    CGContextFillEllipseInRect(context, CGRectMake(touch_x, touch_y, 50, 50));
 }
-*/
+
+- (void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event {
+    for (UITouch *t in touches) {
+        CGPoint p = [t locationInView:self];
+        NSLog(@"Touch at %f, %f", p.x, p.y);
+        touch_x = p.x;
+        touch_y = p.y;
+        [self setNeedsDisplay]; // lets the program know it needs to redraw the screen on press
+    }
+}
 
 @end
