4a38cbaa47958774757e0d756672d90855c8fd0a
Matthew Bradshaw <mbradsh3@binghamton.edu>
2021-03-03 23:33:01 -0500

    fixed bounds for rectangle

--- DrawSwift/DrawSwift/DrawView.swift
+++ DrawSwift/DrawSwift/DrawView.swift
@@ -9,16 +9,24 @@ import UIKit
 
 class DrawView: UIView {
 
+    var interval = 0
     var objx = 0
     var objy = 0
-    var dx = 1
-    var dy = 1
+    var dx = 5
+    var dy = 5
     // Only override draw() if you perform custom drawing.
     // An empty implementation adversely affects performance during animation.
     override func draw(_ rect: CGRect) {
         // Drawing code
+        interval = interval + 1
         let color:UIColor = UIColor.yellow
         let drect = CGRect(x: objx, y:objy, width:50, height:50)
+        if(interval % 200 == 0) {
+            randomRect()
+        }
+        else {
+
+        }
         
         let bpath:UIBezierPath = UIBezierPath(rect: drect)
         
@@ -51,13 +59,20 @@ class DrawView: UIView {
         if objy < Int(self.bounds.minY) {
             dy = -dy
         }
-        if objx > Int(self.bounds.maxX) {
+        if objx > Int(self.bounds.maxX-50) {
             dx = -dx
         }
-        if objy > Int(self.bounds.maxY) {
+        if objy > Int(self.bounds.maxY-50) {
             dy = -dy
         }
         
+        setNeedsDisplay()
+    }
+    
+    @objc func randomRect() {
+        print("calling randomRect")
+        let randRect = CGRect(x: Int.random(in: 1 ..< 300), y: Int.random(in: 1 ..< 550), width: 50, height: 50)
+        UIRectFill(randRect)
         setNeedsDisplay()
     }
     
