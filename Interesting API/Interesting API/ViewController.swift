526e1a488450361997aa4ada8f25b20d0e3b7f61
Matthew Bradshaw <mbradsh3@binghamton.edu>
2021-03-18 15:49:59 -0400

    now works with customization and error checking

--- Interesting API/Interesting API/ViewController.swift
+++ Interesting API/Interesting API/ViewController.swift
@@ -14,20 +14,40 @@ class ViewController: UIViewController {
     @IBOutlet var latitude: UITextField!
     @IBOutlet var longitude: UITextField!
     @IBOutlet var letsGo: UIButton!
+    @IBOutlet var invalid: UILabel!
+    @IBOutlet var validLats: UILabel!
+    @IBOutlet var validLongs: UILabel!
 
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         let startingPoint = CLLocation(latitude: 42.0987, longitude: -75.9180)
         mapView.zoomToLocation(startingPoint)
     }
     
-    @objc func moveLocation() {
-        let newLat = Float(latitude.text!)
-        let newLong = Float(longitude.text!)
-//        let newLocation = CLLocation(latitude: newLat, longitude: newLong)
-//        map.zoomToLocation(newLocation)
+    @IBAction func moveLocation(_ sender: UIButton) {
+        
+        // Everything defaults to the original location's Lat and/or Long if a text field is left blank
+        let newLat = Double(latitude.text!) ?? 42.0987
+        let newLong = Double(longitude.text!) ?? -75.9180
+        
+        var newLocation = CLLocation(latitude: 42.0987, longitude: -75.9180)
+        
+        // If Lat and Long are in the acceptable range, zoom to that location
+        if(newLat > -90 && newLat < 90 && newLong > -180 && newLong < 180) {
+            newLocation = CLLocation(latitude: newLat, longitude: newLong)
+            invalid.text = ""
+            validLats.text = ""
+            validLongs.text = ""
+        }
+        else {
+            invalid.text = "Invalid Lat/Long"
+            validLats.text = "(-90,90) /"
+            validLongs.text = "(-180,180)"
+        }
+                
+        mapView.zoomToLocation(newLocation)
     }
 }
 
@@ -36,6 +56,7 @@ private extension MKMapView {
     _ location: CLLocation,
     regionRadius: CLLocationDistance = 700
   ) {
+    
     let cRegion = MKCoordinateRegion(
       center: location.coordinate,
       latitudinalMeters: regionRadius,
