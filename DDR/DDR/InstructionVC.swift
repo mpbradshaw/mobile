1d33b2eb3bbb8a87a4e098806ebd8c5d4b76ae64
Matthew Bradshaw <mbradsh3@binghamton.edu>
2021-03-29 10:42:04 -0400

    starting on viewcontrollers

---
+++ DDR/DDR/InstructionVC.swift
@@ -0,0 +1,29 @@
+//
+//  InstructionVC.swift
+//  DDR
+//
+//  Created by Matt Bradshaw on 3/29/21.
+//
+
+import UIKit
+
+class InstructionVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ DDR/DDR/TitleView.swift
@@ -0,0 +1,31 @@
+//
+//  TitleView.swift
+//  DDR
+//
+//  Created by Matt Bradshaw on 3/29/21.
+//
+
+import UIKit
+
+class TitleView: UIViewController {
+    @IBOutlet var instructions: UIButton!
+    @IBOutlet var letsPlay: UIButton!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
